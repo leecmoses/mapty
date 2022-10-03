794fa4eefe49f132954a3ceb0c9df74c8bd7cec8
moses <leecmoses@gmail.com>
2022-10-03 10:34:46 -0400

    display map using leaflet library

--- script.js
+++ script.js
@@ -14,8 +14,21 @@ const inputElevation = document.querySelector(".form__input--elevation");
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
-    console.log(latitude, longitude);
     console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    const map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
   },
   function () {
     alert("Could not get your position");
